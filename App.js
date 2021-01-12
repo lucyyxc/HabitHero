a0146c6d239fd30854555bc51c0a8b6ec97798cf
lucyyxc <xilucychen@gmail.com>
2021-01-11 17:17:21 -0800

    created initial interface

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import WelcomeScreen from './app/screens/WelcomeScreen';
+import MonthScreen from './app/screens/MonthScreen';
+import { database } from './app/db/index';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Stack = createStackNavigator();
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: null, // will change to random motivational message
+    };
+    // this.renderStars = this.renderStars.bind(this);
+  }
+
+  // change to useEffect
+  componentDidMount() {
+    // database.getStars();
+    // database.setUpDatabase();
+  }
+
+  // renderStars() {
+  //   database.getStars();
+  // }
+
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name='Welcome' component={WelcomeScreen} />
+          <Stack.Screen
+            name='Month'
+            component={MonthScreen}
+            // renderStars={this.renderStars}
+          />
+        </Stack.Navigator>
+        {/* <WelcomeScreen /> */}
+      </NavigationContainer>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
